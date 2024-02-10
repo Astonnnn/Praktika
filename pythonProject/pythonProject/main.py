63c80cceadce8c2d9a212684ed8d2795b3e55909
Aston Parol <astonparol6@gmail.com>
2024-02-10 12:05:44 +0200

    a

--- pythonProject/pythonProject/main.py
+++ pythonProject/pythonProject/main.py
@@ -1,5 +1,6 @@
 import pygame
 from player import *
+from projectile import *
 
 pygame.init()
 
@@ -14,19 +15,25 @@ floor = pygame.transform.scale(floor, (1280, 380))
 background = pygame.image.load('pildid\scene.jpg')
 
 
-character = Player(500, 500, 130, 150)
+
 
 background = pygame.image.load('pildid\scene.jpg')
 background = pygame.transform.scale(background, (screenWidth, screenHeight))
 
 # siin kõik ekraanile kuvatav
 
+
 def GameWindow():
     screen.blit(background, (0, 0))
     screen.blit(floor, (0, 360))
+    for bullet in bullets:
+        bullet.draw(screen)
     character.draw(screen)
     pygame.display.update()
 
+
+character = Player(500, 500, 130, 150)
+bullets = []
 
 # peaprogramm
 
@@ -36,24 +43,42 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    for bullet in bullets:
+        if 1280 > bullet.x > 0:
+            bullet.x += bullet.vel
+        else:
+            bullets.pop(bullets.index(bullet))
+
     keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_z]:
+        if character.left:
+            facing = -1
+        else:
+            facing = 1
+        if len(bullets) < 16:
+            bullets.append(Projectile(round(character.x + character.width // 2), round(character.y + character.height // 2), 8, (0, 0, 0), facing))
     if keys[pygame.K_LEFT] and character.x > character.vel:
         character.x -= character.vel
         character.left = True
         character.right = False
         character.idleCount = 0
         character.standing = False
+
     elif keys[pygame.K_RIGHT] and character.x < screenWidth - character.width:
         character.x += character.vel
         character.right = True
         character.left = False
         character.idleCount = 0
         character.standing = False
+
     else:
         character.walkCount = 0
         character.standing = True
+
     if not character.isJump:
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_UP]:
             character.isJump = True
             character.walkCount = 0
             character.idleCount = 0
